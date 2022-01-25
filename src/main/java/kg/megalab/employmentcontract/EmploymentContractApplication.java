7cd8ad669b9ab2eb87f84ccbca924a72222ce772
AikaNiyazova <aikaniyazova@gmail.com>
2022-01-25 20:17:13 +0600

    Entities and DTOs are done

---
+++ src/main/java/kg/megalab/employmentcontract/EmploymentContractApplication.java
@@ -0,0 +1,13 @@
+package kg.megalab.employmentcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentContractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megalab/employmentcontract/EmploymentContractApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.employmentcontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
